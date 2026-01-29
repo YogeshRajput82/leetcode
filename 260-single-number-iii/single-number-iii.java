67ecc39fd067dec90709adc6f9cc3dd77ce7660e
Yogesh Rajput <141839046+YogeshRajput82@users.noreply.github.com>
2026-01-30 01:58:01 +0530

    Time: 1 ms (99.61%) | Memory: 48.3 MB (18.92%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor = 0;
+
+        
+        for (int n : nums) {
+            xor ^= n;
+        }
+
+       
+        int diffBit = xor & -xor;
+
+        int a = 0, b = 0;
+
+        
+        for (int n : nums) {
+            if ((n & diffBit) == 0) {
+                a ^= n;
+            } else {
+                b ^= n;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
